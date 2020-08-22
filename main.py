9dbf2824cff63abbe6720c5c1ae4fb0e37b438ea
Konstantin <starbystar@inbox.ru>
2020-08-22 22:38:39 +0500

    add agreement_check func

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import eel
 
 eel.init("web")
 
+# проверка номера договора
+def agreement_check(agr):
+    assert agr.isdigit(), "Номер договора должен быть положительным числом"
+    assert len(str(agr)) == 10, "Номер договора должен состоять из 10 чисел"
+
+#Запуск диагностики при клике на кнопку
 @eel.expose
 def diag_start(agr):
+    agreement_check(agr)
     print(agr)
     return agr
 
