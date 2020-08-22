2c3f31929124e4837f3c54bb90ba9fcb3beb2ba3
Konstantin <starbystar@inbox.ru>
2020-08-22 22:16:23 +0500

    testing js->python interaction some css changes

---
+++ README.md
@@ -0,0 +1,1 @@
+create exe app: Auto PY to EXE
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import eel
 
 eel.init("web")
+
+@eel.expose
+def diag_start(agr):
+    print(agr)
+    return agr
+
 eel.start("main.html", size=(500, 600))
